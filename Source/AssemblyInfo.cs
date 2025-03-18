e15dc4a6c9d554b0721181acfd64f99df0175c93
Luiz Fernando Gemi <luizf.gemi@gmail.com>
2025-03-18 01:00:50 -0300

    release: first pre-release AnchorFixer v0.1.0 🚀

---
+++ Source/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("AnchorFixer")]
+[assembly: AssemblyDescription("Ground Anchor position fixer for KSP")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
